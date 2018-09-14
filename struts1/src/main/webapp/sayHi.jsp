1b3ae7d817aec629427447cc89a6a51ef53437df
zch330 <1357159894@qq.com>
2018-09-14 22:15:51 +0800

    strurs2 ognl访问对象

---
+++ struts1/src/main/webapp/sayHi.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/9/14 0014
+  Time: 18:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>第一个Struts2程序</title>
+</head>
+<body>
+<form action="sayHi.jspsayHi.jsp" method="post">
+    <input type="text" name="username">
+    <input type="submit" value="提交">
+</form>
+</body>
+</html>
---
+++ struts4/src/main/java/cap/action/TimerAction.java
@@ -0,0 +1,31 @@
+package cap.action;
+
+import com.sun.xml.internal.ws.addressing.model.ActionNotSupportedException;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+
+@ParentPackage("login")
+@Namespace("/")
+@InterceptorRefs(@InterceptorRef(value = "mystack"))
+@Results({@Result(name = "success", location = "/index.jsp"),
+        @Result(name = "login", location = "/login.jsp")})
+public class TimerAction extends ActionSupport {
+
+    @Action(value = "count", results = {
+            @Result(name = "success", location = "result.jsp")})
+public class TimerAction extends ActionNotSupportedException {
+
+        @Action(value = "count", results = {
+                @Result(name = "success", location = "result.jsp")
+        })
+
+        public String count() {
+        try {
+            Thread.sleep(500);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return SUCCESS;
+    }
+}
+
